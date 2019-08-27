bc9a582039b0e3928e1f64078c33687e9fc348aa
Suraj Jagdev <suraj_57@hotmail.com>
2019-08-27 14:28:42 -0400

    sending jwt token as headers

--- src/index.js
+++ src/index.js
@@ -2,12 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { ApolloProvider } from 'react-apollo';
-import ApolloClient from 'apollo-boost';
+import {
+  ApolloClient,
+  ApolloLink,
+  InMemoryCache,
+  HttpLink
+} from 'apollo-boost';
 import 'semantic-ui-css/semantic.min.css';
 import * as serviceWorker from './serviceWorker';
-const host = 'http://localhost:8080/graphql';
-const client = new ApolloClient({
+const host = 'http://localhost:3001/graphql';
+const httpLink = new HttpLink({
   uri: host
+});
+const authLink = new ApolloLink((operation, forward) => {
+  // Retrieve the authorization token from local storage.
+  if (localStorage.getItem('token') !== null) {
+    const token = localStorage.getItem('token');
+    const refreshToken = localStorage.getItem('refreshToken');
+    console.log(refreshToken);
+    // Use the setContext method to set the HTTP headers.
+    operation.setContext({
+      headers: {
+        token: token ? `${token}` : '',
+        refreshtoken: refreshToken ? `${refreshToken}` : ''
+        // authorization: token ? `${token}` : ``
+      }
+    });
+  }
+  // Call the next link in the middleware chain.
+  return forward(operation);
+});
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
 });
 ReactDOM.render(
   <ApolloProvider client={client}>

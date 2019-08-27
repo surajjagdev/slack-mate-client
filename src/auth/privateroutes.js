f85e8104c3aba7916acb0fc521962b6e1ec4bfb2
Suraj Jagdev <suraj_57@hotmail.com>
2019-08-27 19:21:10 -0400

    added jwt decode, see if valid jwt is in local storage. private routes

---
+++ src/auth/privateroutes.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import isAuthenticated from './auth.js';
+import { Route, Redirect } from 'react-router-dom';
+const isAuth = true;
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={props => {
+        if (isAuthenticated() === true) {
+          return <Component {...props} />;
+        } else {
+          return (
+            <Redirect
+              to={{
+                pathname: '/',
+                state: {
+                  from: props.location
+                }
+              }}
+            />
+          );
+        }
+      }}
+    />
+  );
+};
+
+export default ProtectedRoute;

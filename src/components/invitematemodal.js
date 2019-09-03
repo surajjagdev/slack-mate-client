f262073f818266c32fe79858251620e8c5ac2f5c
Suraj Jagdev <suraj_57@hotmail.com>
2019-09-03 18:12:52 -0400

    made another modal to add memeber to team with code from add channel modal formil

---
+++ src/components/invitematemodal.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { Form, Input, Button, Modal } from 'semantic-ui-react';
+import { withFormik } from 'formik';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import { flowRight } from 'lodash';
+import errorFormat from '../errorFormat.js';
+const InviteMateModal = ({
+  open,
+  close,
+  values,
+  handleChange,
+  handleBlur,
+  handleSubmit,
+  isSubmitting,
+  touched,
+  errors
+}) => (
+  <Modal open={open} onClose={close}>
+    <Modal.Header>Invite a Mate to Team</Modal.Header>
+    <Modal.Content>
+      <Form>
+        <Form.Field>
+          <Input
+            value={values.name}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            name="email"
+            fluid
+            placeholder="Mate's Email"
+          />
+        </Form.Field>
+        {touched.email && errors.email ? errors.email[0] : null}
+        <Form.Group widths="equal">
+          <Button type="submit" disabled={isSubmitting} fluid onClick={close}>
+            Cancel
+          </Button>
+          <Button
+            type="submit"
+            disabled={isSubmitting}
+            onClick={handleSubmit}
+            fluid
+          >
+            Add Mate
+          </Button>
+        </Form.Group>
+      </Form>
+    </Modal.Content>
+  </Modal>
+);
+
+const addTeamMemberMutation = gql`
+  mutation($email: String!, $teamId: Int!) {
+    addTeamMember(email: $email, teamId: $teamId) {
+      ok
+      errors {
+        path
+        message
+      }
+    }
+  }
+`;
+
+export default flowRight(
+  graphql(addTeamMemberMutation),
+  withFormik({
+    mapPropsToValues: () => ({ email: '' }),
+    handleSubmit: async (
+      values,
+      { props: { close, teamId, mutate }, setSubmitting, setErrors }
+    ) => {
+      const response = await mutate({
+        variables: { teamId, email: values.email }
+      });
+      const { ok, errors } = response.data.addTeamMember;
+      if (ok) {
+        close();
+        setSubmitting(false);
+      } else {
+        setSubmitting(false);
+        setErrors(errorFormat(errors));
+      }
+    }
+  })
+)(InviteMateModal);

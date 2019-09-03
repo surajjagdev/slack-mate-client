71615e66f8e5514c63e7a6fac118841874814cc0
Suraj Jagdev <suraj_57@hotmail.com>
2019-09-03 18:13:31 -0400

    format errors returned when adding team mmebers. Reduce and show first error received

---
+++ src/errorFormat.js
@@ -0,0 +1,24 @@
+//array of errors [{path:"", message:"DNE"}];
+//and create an obj with like
+//{email:['error1', 'error2]}
+/*export default errors => {
+  errors.reduce((acculator, currentValue) => {
+    if (currentValue.path in acculator) {
+      acculator[currentValue.path].push(currentValue.message);
+    } else {
+      acculator[currentValue.path] = [currentValue.message];
+    }
+    return acculator;
+  }, {});
+};*/
+
+export default errors =>
+  errors.reduce((acculator, currentValue) => {
+    if (currentValue.path in acculator) {
+      acculator[currentValue.path].push(currentValue.message);
+    } else {
+      acculator[currentValue.path] = [currentValue.message];
+    }
+
+    return acculator;
+  }, {});

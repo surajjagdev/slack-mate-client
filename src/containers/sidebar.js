238ea27cf1dd3dc3d6653058944bbcb173c49376
Suraj Jagdev <suraj_57@hotmail.com>
2019-09-03 18:12:16 -0400

    sidebar

--- src/containers/sidebar.js
+++ src/containers/sidebar.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import decode from 'jwt-decode';
 import AddChannelModal from '../components/addchannelmodal.js';
+import InviteMateModal from '../components/invitematemodal.js';
 import Channels from '../components/channels.js';
 import Teams from '../components/teams.js';
 
 export default class Sidebar extends React.Component {
   state = {
-    openAddChannelModal: false
+    openAddChannelModal: false,
+    openAddInviteModal: false
   };
   handleAddChannel = () => {
     console.log('handleaddchannel');
     this.setState(prevState => ({
       openAddChannelModal: !prevState.openAddChannelModal
+    }));
+  };
+  handleInviteMate = () => {
+    console.log('handleadd mate');
+    this.setState(prevState => ({
+      openAddInviteModal: !prevState.openAddInviteModal
     }));
   };
   render() {
@@ -34,15 +42,21 @@ export default class Sidebar extends React.Component {
         userName={username}
         teamId={team.id}
         channels={team.channels}
-        addChannel={this.addChannel}
         users={[{ id: 1, name: 'slackbot' }, { id: 2, name: 'user1' }]}
         addChannel={this.handleAddChannel}
+        handleInviteMate={this.handleInviteMate}
       />,
       <AddChannelModal
         key="addchannelmodal-sidebar"
         open={this.state.openAddChannelModal}
         teamId={parseInt(team.id, 10)}
         close={this.handleAddChannel}
+      />,
+      <InviteMateModal
+        key="invitematemodal-sidebar"
+        open={this.state.openAddInviteModal}
+        teamId={parseInt(team.id, 10)}
+        close={this.handleInviteMate}
       />
     ];
   }

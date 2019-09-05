3aff9cc5100dec3e27f3092d58a111fd92867745
Suraj Jagdev <suraj_57@hotmail.com>
2019-09-05 17:04:49 -0400

    sending messages frontend and error handling

--- src/components/input.js
+++ src/components/input.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Input } from 'semantic-ui-react';
+import { withFormik } from 'formik';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import { flowRight } from 'lodash';
+
 const InputWrapper = styled.div`
   background-color: #362234;
   grid-row: 3;
@@ -8,11 +13,51 @@ const InputWrapper = styled.div`
   margin: 20px;
   color: 'black';
 `;
-
-export default ({ channelName }) => {
-  return (
-    <InputWrapper>
-      <Input fluid placeholder={`Message #${channelName || 'fill'}`} />
-    </InputWrapper>
-  );
-};
+const sendMessageMutation = gql`
+  mutation($channelId: Int!, $text: String!) {
+    createMessage(channelId: $channelId, text: $text)
+  }
+`;
+const SendMessage = ({
+  channelName,
+  values,
+  handleChange,
+  handleBlur,
+  handleSubmit,
+  isSubmitting
+}) => (
+  <InputWrapper>
+    <Input
+      onKeyDown={e => {
+        if (e.keyCode === 13 && !isSubmitting) {
+          handleSubmit();
+        }
+      }}
+      name="message"
+      value={values.message}
+      onBlur={handleBlur}
+      onChange={handleChange}
+      fluid
+      placeholder={`Message ${channelName}`}
+    />
+  </InputWrapper>
+);
+export default flowRight(
+  graphql(sendMessageMutation),
+  withFormik({
+    mapPropsToValues: () => ({ message: '' }),
+    handleSubmit: async (
+      values,
+      { props: { channelId, mutate }, setSubmitting, resetForm }
+    ) => {
+      //if message doesnt exist dont submit
+      if (!values.message || values.message.trim()) {
+        setSubmitting(false);
+      }
+      await mutate({
+        variables: { channelId, text: values.message }
+      });
+      resetForm(false);
+    }
+  })
+)(SendMessage);

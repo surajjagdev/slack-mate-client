9faa9fe01438219fa7acae9205c18d05e88abe2b
Suraj Jagdev <suraj_57@hotmail.com>
2019-09-06 13:04:18 -0400

    added in padding left adn right, and showing messages most recent at bottom

--- src/components/messages.js
+++ src/components/messages.js
@@ -2,4 +2,9 @@ import styled from 'styled-components';
 export default styled.div`
   grid-row: 2;
   grid-column: 3;
+  padding-left: 20px;
+  padding-right: 20px;
+  display: flex;
+  flex-direction: column-reverse;
+  overflow-y: auto;
 `;

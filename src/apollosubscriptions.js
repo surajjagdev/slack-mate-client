9130a2e1023436508ed96e79bdaf7dd6b29c7926
Suraj Jagdev <suraj_57@hotmail.com>
2019-09-06 14:11:51 -0400

    moved all graphql connections and also added in ws connection

---
+++ src/apollosubscriptions.js
@@ -0,0 +1,74 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloLink,
+  split,
+  HttpLink
+} from 'apollo-boost';
+//import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+const host = 'http://localhost:3001/graphql';
+const wsHost = 'ws://localhost:3001/subscriptions';
+const httpLink = new HttpLink({
+  uri: host
+});
+const middlewareLink = setContext(() => ({
+  headers: {
+    token: localStorage.getItem('token') || null,
+    refreshtoken: localStorage.getItem('refreshToken') || null
+  }
+}));
+const afterWareLink = new ApolloLink((operation, forward) => {
+  return forward(operation).map(response => {
+    const context = operation.getContext();
+    const {
+      response: { headers }
+    } = context;
+
+    if (headers) {
+      const token = headers.get('token');
+      const refreshToken = headers.get('refreshtoken');
+
+      if (token) {
+        localStorage.setItem('token', token);
+      }
+
+      if (refreshToken) {
+        localStorage.setItem('refreshToken', refreshToken);
+      }
+    }
+    return response;
+  });
+});
+/*const logoutLink = onError(({ networkError }) => {
+  if (networkError.statusCode === 401) {
+    console.log('netwrok erro');
+  }
+});*/
+const httpLinkWithMiddleware = afterWareLink.concat(
+  middlewareLink.concat(httpLink)
+);
+//ws
+const wsLink = new WebSocketLink({
+  uri: wsHost,
+  options: {
+    reconnect: true
+  }
+});
+//split the connections between ws and (authmiddleware, middleware and httplink)
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  httpLinkWithMiddleware
+);
+
+export default new ApolloClient({
+  link,
+  //link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});

a0dc490ba2f76979a8c59a63154d47f6d6593036
Suraj Jagdev <suraj_57@hotmail.com>
2019-09-23 15:24:23 -0400

    dont need references, or route to direct messages

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,6 @@ import Register from './register.js';
 import Login from './login.js';
 import CreateTeam from './createTeam.js';
 import ViewTeam from './viewTeam.js';
-import DirectMessages from './directMessages.js';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ProtectedRoute from '../auth/privateroutes.js';
 const Routes = () => {
@@ -20,11 +19,6 @@ const Routes = () => {
           component={ViewTeam}
         />
         <ProtectedRoute exact path="/createteam" component={CreateTeam} />
-        <ProtectedRoute
-          exact
-          path="/viewteam/user/:teamId/:userId?"
-          component={DirectMessages}
-        />
       </Switch>
     </Router>
   );
